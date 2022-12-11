981d868f3489225638d8f6872b65e7267e40b3c7
Egek0py <egek.922@gmail.com>
2022-12-11 17:20:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,76 @@
+#!/usr/bin/env python
+
+import threading
+
+import string
+
+import base64
+
+import urllib.request
+
+import urllib.parse
+
+import os
+
+import time
+
+import sys
+
+import random
+
+try:
+
+    import requests
+
+except ImportError:
+
+    os.system('pip2 install requests')
+
+
+
+try:
+
+    request = requests.get("https://www.google.com/search?q=sms+intcontrol", timeout=3)
+
+except (requests.ConnectionError, requests.Timeout) as exception:
+
+    print("[!] No internet connection [!]")
+
+    sys.exit()
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+yellow='\033[93m'
+
+gren='\033[92m'
+
+cyan='\033[96m'
+
+pink='\033[95m'
+
+red='\033[91m'
+
+b='\033[1m'
+
+W = '\033[0m'
+
+colors = ['\033[1;31m', '\033[1;32m', '\033[1;33m', '\033[1;34m', '\033[1;35m', '\033[1;36m']
+
+
+
+def clr():
+
+    if os.name == 'nt':
+
+        os.system('cls')
+
+    else:
+
+        os.system('clear')
+
 
 def banner():
 
