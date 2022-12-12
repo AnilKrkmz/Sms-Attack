a2fa8f838a276d1275f2574b8d9388c26c38a315
Egek0py <egek.922@gmail.com>
2022-12-12 21:12:31 +0300

    Update defRequest.py

--- defRequest.py
+++ defRequest.py
@@ -10,7 +10,7 @@ payload =  {
 """
 
 #Requests Kütüphanesini tanımlayalım
-
+"""
 import requests
 
 # Telefon doğrulama postu için gerekli bilgileri tanımlayın
@@ -28,3 +28,23 @@ if response.status_code == 200:
     print('Telefon doğrulama postu başarıyla gönderildi!')
 else:
     print('Telefon doğrulama postu gönderilemedi.')
+
+
+
+"""
+
+import requests
+
+url = "https://api.gotinder.com/v2/auth/sms/send"
+
+payload = {
+    "auth_type": "sms",
+    "locale": "tr",
+    "phone_number": "{+90}{5074204209}"
+}
+
+
+
+response = requests.request("POST", url, data = payload)
+
+print(response.text.encode('utf8'))
