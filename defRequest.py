b47d94561983c7641c6ad65a072a2bf7ed6c2938
Nuri Anıl Korkmaz <anilkorkmaz2011@outlook.com>
2022-12-16 19:11:35 +0300

    Update defRequest.py

--- defRequest.py
+++ defRequest.py
@@ -1,50 +1,5 @@
-"""
-
-import requests
-
-tinderAPi_Url = "https://api.gotinder.com/v3/auth/login?locale=en-GB"
-
-payload =  {
-    
-}
-"""
-
-#Requests Kütüphanesini tanımlayalım
-"""
-import requests
-
-# Telefon doğrulama postu için gerekli bilgileri tanımlayın
-url = 'https://api.gotinder.com/v3/auth/login?locale=en-GB'
-data = {
-    'phone_number': '+90 534 633 09 53',
-    
-}
-
-# Post isteğini gönderir
-response = requests.post(url, data=data)
-
-# Sonuçları kontrol eder
-if response.status_code == 200:
-    print('Telefon doğrulama postu başarıyla gönderildi!')
-else:
-    print('Telefon doğrulama postu gönderilemedi.')
-
-
-
-"""
-
-import requests
-
-url = "https://api.gotinder.com/v2/auth/sms/send"
-
-payload = {
-    "auth_type": "sms",
-    "locale": "tr",
-    "phone_number": "{+90}{5074204209}"
-}
-
-
-
-response = requests.request("POST", url, data = payload)
-
-print(response.text.encode('utf8'))
+from textmagic.rest import TextmagicRestClient
+username = "your_textmagic_username"
+token = "your_apiv2_key"
+client = TextmagicRestClient(username, token)
+message = client.messages.create(phones="+905346330953", text="Hello TextMagic")
